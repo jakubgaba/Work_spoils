ad9806830ab5c25b52e00345fa61ad1bb8ceedf8
Jakub <j.gaba@email.cz>
2024-08-12 21:41:48 +0200

    Adding_folders

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,21 @@
+
+<script>
+	console.log("Iam angry")
+</script>
+
+<svelte:head>
+	<title>AngryEmployee demo app</title>
+</svelte:head>
+
 <nav>
 	<a href="/">home</a>
 	<a href="/about">about</a>
 </nav>
+
+<style>
+	:global(body) {
+		font-family: 'Open Sans', sans-serif;
+	}
+</style>
 
 <slot />
