90f6d1199b81d067fddb4b4c5c2abeb08c1d94f5
Jakub <j.gaba@email.cz>
2024-08-26 22:49:32 +0200

    AddedTODO

--- backend/service.go
+++ backend/service.go
@@ -1,3 +1,12 @@
+// TODO: DB filename
+//NOTE: Something easy
+// TODO: Main /GET/ function for DB
+//NOTE: Before this step make sure you have created a DB :)
+//TODO: Adding an option of order in a DB
+//NOTE: Playing around with descending and ascending blah blah
+//TODO: Write reach order line as a row
+//TODO: New /GET/ function but this time also /POST/ function
+
 package backend
 
 import (
