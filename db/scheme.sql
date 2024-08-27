30a8c1b7f7792a040c2388fc42ae7d47fd13567e
Jakub <j.gaba@email.cz>
2024-08-27 21:30:20 +0200

    AddingDB

---
+++ db/scheme.sql
@@ -0,0 +1,22 @@
+CREATE TABLE "test" (
+    "id" INTEGER NOT NULL UNIQUE, -- unique ID test
+    "name" TEXT NOT NULL,
+    "location" TEXT NOT NULL,
+    PRIMARY KEY ("id")
+);
+
+INSERT INTO "main"."products"(
+    "id",
+    "name",
+    "location"
+)
+VALUES
+    ('1','Test_1','Russia'),
+    ('2','Test_2','Canada'),
+    ('3','Test_3','Nigeria'),
+    ('4','Test_4','Poland'),
+    ('5','Test_5','Russia'),
+    ('6','Test_6','Nigeria'),
+    ('7','Test_7','Nigeria'),
+    ('8','Test_8','UK');
+
